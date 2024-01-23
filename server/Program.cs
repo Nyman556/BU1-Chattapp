d91730cd140d7166d5bfc56f943ed7952cc58694
Johan Nyman <johan.nyman97@gmail.com>
2024-01-23 11:28:39 +0100

    generell setup av server sockets

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,7 @@
-﻿using ServerTests;
+﻿using System.Net;
+using System.Net.Sockets;
+
+using ServerTests;
 
 namespace server;
 
@@ -6,6 +9,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Socket> sockets = new List<Socket>();
+        IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
+        IPEndPoint iPEndPoint = new IPEndPoint(ipAddress, 25500);
+
+        Socket serverSocket = new Socket(
+            ipAddress.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp
+        );
+
+        serverSocket.Bind(iPEndPoint);
+        serverSocket.Listen(5);
+
+        while (true)
+        {
+            if (serverSocket.Poll(0, SelectMode.SelectRead))
+            {
+                Socket client = serverSocket.Accept();
+                Console.WriteLine("A client has connected!");
+                sockets.Add(client);
+            }
+
+            foreach (Socket client in sockets)
+            {
+                // Blockar inte koden om det inte finns något att läsa.
+                if (client.Poll(0, SelectMode.SelectRead))
+                {
+                    byte[] incoming = new byte[5000];
+                    int read = client.Receive(incoming);
+                    string message = System.Text.Encoding.UTF8.GetString(incoming, 0, read);
+                    Console.WriteLine("From a client: " + message);
+                }
+            }
+        }
     }
 }
