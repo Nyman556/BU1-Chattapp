858609bc478954a8e50bfc7265cac913c9621838
Pjohanssonswe <p.johansson.swe@gmail.com>
2024-01-26 11:11:28 +0100

    fix some notification

--- server/Program.cs
+++ server/Program.cs
@@ -8,6 +8,11 @@ namespace server;
 
 class Program
 {
+    static List<Socket> sockets = new List<Socket>();
+    static Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    //static Socket serverSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
     static void Main(string[] args)
     {
         MongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
@@ -15,7 +20,6 @@ class Program
 
         // Hämta eller skapa en samling för användare
         var usersCollection = database.GetCollection<UserModel>("users");
-
         var filter = Builders<UserModel>.Filter.Empty;
         List<UserModel> allUsers = usersCollection.Find(filter).ToList();
 
@@ -23,47 +27,67 @@ class Program
         {
             Console.WriteLine(user.Username);
         }
-        //var test;
-        List<Socket> sockets = new List<Socket>();
+        
+        SetupServer();
+        AcceptClients();
+    }
+
+        static void SetupServer()
+        {
         IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
         IPEndPoint iPEndPoint = new IPEndPoint(ipAddress, 25500);
-
-        Socket serverSocket = new Socket(
-            ipAddress.AddressFamily,
-            SocketType.Stream,
-            ProtocolType.Tcp
-        );
+        
+        //Socket serverSocket = new Socket(
+        //    ipAddress.AddressFamily,
+        //    SocketType.Stream,
+        //    ProtocolType.Tcp
+        //);
 
         serverSocket.Bind(iPEndPoint);
         serverSocket.Listen(5);
 
-        while (true)
+        Console.WriteLine("Server is running on" + iPEndPoint);
+        }
+        static void AcceptClients()
         {
-            if (serverSocket.Poll(0, SelectMode.SelectRead))
+        while (true)
             {
-                Socket client = serverSocket.Accept();
-                Console.WriteLine("A client has connected!");
-                sockets.Add(client);
-            }
-
-            foreach (Socket client in sockets)
-            {
-                // Blockar inte koden om det inte finns något att läsa.
-                if (client.Poll(0, SelectMode.SelectRead))
+                if (serverSocket.Poll(0, SelectMode.SelectRead))
                 {
-                    byte[] incoming = new byte[5000];
-                    int read = client.Receive(incoming);
-                    string message = System.Text.Encoding.UTF8.GetString(incoming, 0, read);
-                    Console.WriteLine("From a client: " + message);
+                    Socket client = serverSocket.Accept();
+                    //Console.WriteLine("A client has connected!" );
+                    sockets.Add(client);
+                    BroadcastNotification("A new client has connected! from " + client.RemoteEndPoint);
+                }
+                
+                foreach (Socket client in sockets)
+                {
+                    // Blockar inte koden om det inte finns något att läsa.
+                    if (client.Poll(0, SelectMode.SelectRead))
+                    {
+                        byte[] incoming = new byte[5000];
+                        int read = client.Receive(incoming);
+                        string message = System.Text.Encoding.UTF8.GetString(incoming, 0, read);
+                        Console.WriteLine("From a client: " + message);
+                    }
                 }
             }
         }
+
+        static void BroadcastNotification(string message)
+        {
+            foreach (Socket client in sockets)
+            {
+                byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
+                client.Send(messageBytes);
+            }
+        }    
     }
-}
+
 
 class UserModel
 {
     public ObjectId _id { get; set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
 }
