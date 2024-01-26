f43212c6e19d9b17db4436906330fe2ba9ac4117
Pjohanssonswe <p.johansson.swe@gmail.com>
2024-01-26 11:47:54 +0100

    fix more notification

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Linq.Expressions;
+using System.Net;
 using System.Net.Sockets;
 using MongoDB.Bson;
 using MongoDB.Driver;
@@ -27,16 +28,16 @@ class Program
         {
             Console.WriteLine(user.Username);
         }
-        
+
         SetupServer();
         AcceptClients();
     }
 
-        static void SetupServer()
-        {
+    static void SetupServer()
+    {
         IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
         IPEndPoint iPEndPoint = new IPEndPoint(ipAddress, 25500);
-        
+
         //Socket serverSocket = new Socket(
         //    ipAddress.AddressFamily,
         //    SocketType.Stream,
@@ -47,42 +48,61 @@ class Program
         serverSocket.Listen(5);
 
         Console.WriteLine("Server is running on" + iPEndPoint);
-        }
-        static void AcceptClients()
-        {
+    }
+    static void AcceptClients()
+    {
         while (true)
-            {
-                if (serverSocket.Poll(0, SelectMode.SelectRead))
-                {
-                    Socket client = serverSocket.Accept();
-                    //Console.WriteLine("A client has connected!" );
-                    sockets.Add(client);
-                    BroadcastNotification("A new client has connected! from " + client.RemoteEndPoint);
-                }
-                
-                foreach (Socket client in sockets)
-                {
-                    // Blockar inte koden om det inte finns något att läsa.
-                    if (client.Poll(0, SelectMode.SelectRead))
-                    {
-                        byte[] incoming = new byte[5000];
-                        int read = client.Receive(incoming);
-                        string message = System.Text.Encoding.UTF8.GetString(incoming, 0, read);
-                        Console.WriteLine("From a client: " + message);
-                    }
-                }
-            }
-        }
-
-        static void BroadcastNotification(string message)
         {
+            if (serverSocket.Poll(0, SelectMode.SelectRead))
+            {
+                Socket client = serverSocket.Accept();
+                //Console.WriteLine("A client has connected!" );
+                sockets.Add(client);
+                BroadcastNotification("A new client has connected! from " + client.RemoteEndPoint);
+
+
+                try
+                {
+                    byte[] incoming = new byte[5000];
+                    int read = client.Receive(incoming);
+
+                    if (read == 0)
+                    {
+                        BroadcastNotification("A client has disconnected" + client.RemoteEndPoint);
+                    }
+                    else
+                    {
+                        string message = System.Text.Encoding.UTF8.GetString(incoming, 0, read);
+                        Console.WriteLine("From Client " + client.RemoteEndPoint + message);
+                    }                
+                } catch(SocketException ex)
+                {
+                    Console.WriteLine("Exception"+ ex);
+                }
+                }
             foreach (Socket client in sockets)
             {
-                byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
-                client.Send(messageBytes);
+                // Blockar inte koden om det inte finns något att läsa.
+                if (client.Poll(0, SelectMode.SelectRead))
+                {
+                    byte[] incoming = new byte[5000];
+                    int read = client.Receive(incoming);
+                    string message = System.Text.Encoding.UTF8.GetString(incoming, 0, read);
+                    Console.WriteLine("From a client: " + message);
+                }
             }
-        }    
+        }
     }
+
+    static void BroadcastNotification(string message)
+    {
+        foreach (Socket client in sockets)
+        {
+            byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
+            client.Send(messageBytes);
+        }
+    }
+}
 
 
 class UserModel
