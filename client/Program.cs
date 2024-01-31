aa64509263bea9fdb02f3ee679f9046450c63229
Oliverholm <oliverxholmberg@gmail.com>
2024-01-31 09:28:47 +0100

    Global message feature

--- client/Program.cs
+++ client/Program.cs
@@ -7,23 +7,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
-        IPEndPoint iPEndPoint = new IPEndPoint(ipAddress, 25500);
+        List<User> activeUsers = new List<User>();
+        //skicka meddelandet till aktiva användare
+        SendMessageToActiveUsers(message);
 
-        Socket clientSocket = new Socket(
-            ipAddress.AddressFamily,
-            SocketType.Stream,
-            ProtocolType.Tcp
-        );
+        private static void SendMessageToActiveUsers(string message)
+        {
+            byte[] buffer = System.Text.Encoding.ASCII.GetBytes(message);
 
-        clientSocket.Connect(iPEndPoint);
+            foreach (User user in activeUsers)
+            {
+                user.Socket.Send(buffer);
+            }
+        }
+    class User
+    {
+        public Socket Socket { get; }
+
+        public User(Socket socket)
+        {
+            Socket = socket;
+        }
+    }
+
+    IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
+    IPEndPoint iPEndPoint = new IPEndPoint(ipAddress, 25500);
+
+    Socket clientSocket = new Socket(
+        ipAddress.AddressFamily,
+        SocketType.Stream,
+        ProtocolType.Tcp
+    );
+
+    clientSocket.Connect(iPEndPoint);
 
         while (true)
         {
             string message = Console.ReadLine()!;
-            byte[] buffer = System.Text.Encoding.ASCII.GetBytes(message);
+    byte[] buffer = System.Text.Encoding.ASCII.GetBytes(message);
 
-            clientSocket.Send(buffer);
+    clientSocket.Send(buffer);
         }
     }
 }
